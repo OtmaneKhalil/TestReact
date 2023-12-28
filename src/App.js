1ceddbf0bdd6f26413823dfe66c964e58e3be50f
otmane <otmanekhalil012@gmail.com>
2023-12-28 15:52:50 +0100

    Déposer le projet en GitHub

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <Count/>
+    </div>
+  );
+}
+
+
+// Ici j'ai crée le composant Count
+const Count = () => {
+
+  // j'ai utilsé ici le useState pour stocker la variable count localement, et j'ai l'inisialisé par 0.
+  const [count, setCount] = useState(0); 
+
+  // la fonction increment pour incrémenter la variable count
+  const increment = () => {
+    setCount(count + 1);
+  }
+
+  // la fonction decrement pour décrémenter la varaibla count
+  const decrement = () => {
+    setCount(count - 1);
+  }
+
+  // ici j'ai défini une varible pour stocker le nom de la class (soit blue ou rouge)
+  var numberColor = '';
+
+  // ici je test si le numéro (count) devient supérieure à 0, on utilise la class numberBlue pour afficher le numéro en blue.
+  // sinon si le numéro devient inférieure à 0 on l'affiche en rouge.
+  if (count > 0) {
+    numberColor = 'numberBlue';
+  } else if (count < 0) {
+    numberColor = 'numberRed';
+  }
+
+  //ici le html du composant Count.
+  return (
+    <div>
+      <h1 className={numberColor} >{count}</h1> 
+      <div>
+        <button className='btn btn-decrement' onClick={decrement}>-</button>
+        <button className='btn btn-increment' onClick={increment}>+</button>
+      </div>
     </div>
   );
 }
